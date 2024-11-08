7e0a428fe643d6ebedd560b080f11c3bd0171848
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-07 19:26:17 -0500

    1

--- apps/web/src/app/docs/api/_layout.tsx
+++ apps/web/src/app/docs/api/_layout.tsx
@@ -13,10 +13,6 @@ function Menu() {
       <ul className="mt-4 flex flex-col gap-y-2 border-r border-appMenu">
         <Link href="/docs/api/match-location">matchLocation</Link>
         <Link href="/docs/api/outlet">Outlet</Link>
-        <Link href="/docs/api/use-link">useLink</Link>
-        <Link href="/docs/api/use-location">useLocation</Link>
-        <Link href="/docs/api/use-match">useMatch</Link>
-        <Link href="/docs/api/use-navigate">useNavigate</Link>
         <Link href="/docs/api/_content">_content.tsx</Link>
         <Link href="/docs/api/_layout">_layout.tsx</Link>
         <Link href="/docs/api/_void">_void.tsx</Link>
--- apps/web/src/app/docs/api/content.md
+++ apps/web/src/app/docs/api/content.md
@@ -8,13 +8,6 @@
 
 - [Outlet](/docs/api/outlet)
 
-## Hooks
-
-- [useLink](/docs/api/use-link)
-- [useLocation](/docs/api/use-location)
-- [useMatch](/docs/api/use-match)
-- [useNavigate](/docs/api/use-navigate)
-
 ## Routes
 
 - [\_content.tsx](/docs/api/_content)
