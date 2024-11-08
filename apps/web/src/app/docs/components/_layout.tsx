80a943b47d0e7598fc70803700271fd22f26ba10
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-07 19:33:36 -0500

    docs: components

--- apps/web/src/app/docs/components/_layout.tsx
+++ apps/web/src/app/docs/components/_layout.tsx
@@ -11,10 +11,7 @@ function Menu() {
   return (
     <div className="mt-4 border-t border-appBorder">
       <ul className="mt-4 flex flex-col gap-y-2 border-r border-appMenu">
-        <Link href="/docs/api/match-location">matchLocation</Link>
-        <Link href="/docs/api/_content">_content.tsx</Link>
-        <Link href="/docs/api/_layout">_layout.tsx</Link>
-        <Link href="/docs/api/_void">_void.tsx</Link>
+        <Link href="/docs/api/outlet">Outlet</Link>
       </ul>
     </div>
   );
