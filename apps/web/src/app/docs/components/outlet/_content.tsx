aa5d35c159ac111e7c57327e2080669e8bc38682
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-09 21:26:36 -0500

    1

--- apps/web/src/app/docs/components/outlet/_content.tsx
+++ apps/web/src/app/docs/components/outlet/_content.tsx
@@ -1,5 +1,15 @@
 import Content from "./content.md";
 
 export default {
-  Content,
+  Docs,
 };
+
+function Docs() {
+  return (
+    <>
+      <title>enrouter docs | Outlet component</title>
+      <meta name="description" content="File-based router library for React" />
+      <Content />
+    </>
+  );
+}
