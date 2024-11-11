d6abb0987fc3c5dd017a136058b5e10d877a68ba
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-10 22:01:58 -0500

    1

--- apps/web/src/app/docs/start/_content.tsx
+++ apps/web/src/app/docs/start/_content.tsx
@@ -9,6 +9,7 @@ function Docs() {
     <>
       <title>enrouter docs | Get Started</title>
       <meta name="description" content="File-based router library for React" />
+      <link rel="canonical" href="https://enrouter.dev/docs/start" />
       <Content />
     </>
   );
