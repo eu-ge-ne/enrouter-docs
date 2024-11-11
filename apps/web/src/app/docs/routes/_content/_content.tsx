628bb2c12aa94eb726774931d1309f16c8fc45c5
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-10 21:35:59 -0500

    1

--- apps/web/src/app/docs/routes/_content/_content.tsx
+++ apps/web/src/app/docs/routes/_content/_content.tsx
@@ -9,6 +9,7 @@ function Docs() {
     <>
       <title>enrouter docs | _content route</title>
       <meta name="description" content="File-based router library for React" />
+      <link rel="canonical" href="https://enrouter.dev/docs/routes/_content" />
       <Content />
     </>
   );
--- apps/web/src/app/docs/routes/match-location/_content.tsx
+++ apps/web/src/app/docs/routes/match-location/_content.tsx
@@ -9,6 +9,10 @@ function Docs() {
     <>
       <title>enrouter docs | matchLocation</title>
       <meta name="description" content="File-based router library for React" />
+      <link
+        rel="canonical"
+        href="https://enrouter.dev/docs/routes/match-location"
+      />
       <Content />
     </>
   );
