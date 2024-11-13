2448b82d51d99112952c06c7c8a4d553b780d203
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-11-13 18:32:29 -0500

    1

--- apps/web/src/app/docs/vite/_content.tsx
+++ apps/web/src/app/docs/vite/_content.tsx
@@ -8,7 +8,10 @@ function Docs() {
   return (
     <>
       <title>enrouter docs | Vite</title>
-      <meta name="description" content="File-based router library for React" />
+      <meta
+        name="description"
+        content="enrouter comes with Vite plugin for generating routes from file system"
+      />
       <link rel="canonical" href="https://enrouter.dev/docs/vite" />
       <Content />
     </>
