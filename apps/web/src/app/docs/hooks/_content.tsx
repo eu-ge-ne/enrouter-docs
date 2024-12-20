12b76790b5e75e2a18db00ea015707a3340cdbd0
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-12-19 19:16:29 -0500

    fix: meta tag for hooks

--- apps/web/src/app/docs/hooks/_content.tsx
+++ apps/web/src/app/docs/hooks/_content.tsx
@@ -8,7 +8,10 @@ function Docs() {
   return (
     <>
       <title>enrouter docs | Hooks</title>
-      <meta name="description" content="File-based router library for React" />
+      <meta
+        name="description"
+        content="Explore the React hooks available in enrouter, including useActive, useLink, useLocation, useMatch, useMatchFor, and useNavigate. Simplify navigation, location handling, and route matching in your application."
+      />
       <link rel="canonical" href="https://enrouter.dev/docs/hooks" />
       <Content />
     </>
