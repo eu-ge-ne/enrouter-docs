9ee2e15b901b1f13ddf2659945d619a1f8d563d7
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-12-21 12:38:16 -0500

    fix: useLink

--- apps/web/src/app/docs/hooks/use-link/_content.tsx
+++ apps/web/src/app/docs/hooks/use-link/_content.tsx
@@ -8,7 +8,10 @@ function Docs() {
   return (
     <>
       <title>enrouter docs | useLink hook</title>
-      <meta name="description" content="File-based router library for React" />
+      <meta
+        name="description"
+        content="Learn about the useLink hook in enrouter. Create links programmatically by generating href and onClick props for anchor elements or navigation components."
+      />
       <link rel="canonical" href="https://enrouter.dev/docs/hooks/use-link" />
       <Content />
     </>
