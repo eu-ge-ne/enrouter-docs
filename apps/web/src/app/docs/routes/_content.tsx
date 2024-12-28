bc6d8b3e0ad2189b7fcb52af59542302856f7cb9
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2024-12-27 19:13:57 -0500

    fix: routes (#50)

--- apps/web/src/app/docs/routes/_content.tsx
+++ apps/web/src/app/docs/routes/_content.tsx
@@ -8,7 +8,10 @@ function Docs() {
   return (
     <>
       <title>enrouter docs | Routes</title>
-      <meta name="description" content="File-based router library for React" />
+      <meta
+        name="description"
+        content="Learn how to define routes in your application using a tree structure of folders and files. Understand the role of _layout.tsx, _content.tsx, and _fallback.tsx files in creating nested and dynamic routes."
+      />
       <link rel="canonical" href="https://enrouter.dev/docs/routes" />
       <Content />
     </>
