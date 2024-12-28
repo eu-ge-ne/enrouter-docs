e7117acb2109ec00a95bc4ef05a61fa256e74d34
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-12-27 19:12:17 -0500

    1

--- apps/web/src/app/docs/components/_content.tsx
+++ apps/web/src/app/docs/components/_content.tsx
@@ -8,7 +8,10 @@ function Docs() {
   return (
     <>
       <title>enrouter docs | Components</title>
-      <meta name="description" content="Discover enrouter’s minimalistic approach to routing components. Learn about StaticRouter, BrowserRouter, and Outlet components, designed for efficient server and browser routing with seamless child route rendering." />
+      <meta
+        name="description"
+        content="Discover enrouter’s minimalistic approach to routing components. Learn about StaticRouter, BrowserRouter, and Outlet components, designed for efficient server and browser routing with seamless child route rendering."
+      />
       <link rel="canonical" href="https://enrouter.dev/docs/components" />
       <Content />
     </>
