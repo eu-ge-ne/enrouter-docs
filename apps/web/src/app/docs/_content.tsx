abda4d0790e81091fd0c426a83b7c9bafefc5829
Eugene Pobochny <eugene.pobochny@gmail.com>
2024-12-31 21:07:18 -0500

    fix: docs

--- apps/web/src/app/docs/_content.tsx
+++ apps/web/src/app/docs/_content.tsx
@@ -8,7 +8,10 @@ function Docs() {
   return (
     <>
       <title>enrouter | Docs</title>
-      <meta name="description" content="File-based router library for React" />
+      <meta
+        name="description"
+        content="Explore enrouter, a modern React routing library and Vite plugin for seamless file-based routing with “named outlets.” Written in TypeScript, it offers efficient routing for React 18+ and Vite 6+. Learn more and get started!"
+      />
       <link rel="canonical" href="https://enrouter.dev/docs" />
       <Content />
     </>
